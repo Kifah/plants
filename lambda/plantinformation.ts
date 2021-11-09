f2ec5cb1fc8891bb52a209fe77f1ac5eab64876f
Kifah Abbad <kifah@web.de>
2021-11-09 15:11:35 +0100

    lambda has access to configs

--- lambda/plantinformation.ts
+++ lambda/plantinformation.ts
@@ -1,6 +1,28 @@
+import * as AWS from 'aws-sdk';
+
+
+declare const process: {
+    env: {
+        AWS_APPCONFIG_REF: string; // application id
+        AWS_APPCONFIG_PLANT_NAME: string; // enviornment in AppConfig Application
+    };
+};
+
+const appConfig = new AWS.AppConfig({apiVersion: '2019-10-09'});
+
 export const handler = (
         event: any
     ): any => {
-        console.log('hello plants');
+        const params = {
+            MaxResults: 5,
+        };
+        appConfig.listApplications(params, function (err, data) {
+            console.log('application data...');
+            if (err) console.log(err, err.stack);
+            else console.log(data);
+        });
+        console.log('debugging plants configuration...');
+        console.log('application ref', process.env.AWS_APPCONFIG_REF);
+        console.log('application env', process.env.AWS_APPCONFIG_PLANT_NAME);
     }
 ;
--- lib/plants-stack.ts
+++ lib/plants-stack.ts
@@ -1,14 +1,54 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
+import {CfnParameter} from "@aws-cdk/core";
+import {Effect, PolicyStatement} from '@aws-cdk/aws-iam';
+
+import {
+    CfnApplication,
+    CfnEnvironment,
+    CfnConfigurationProfile,
+    CfnHostedConfigurationVersion
+} from "@aws-cdk/aws-appconfig";
 
 export class PlantsStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    const handler = new lambda.Function(this, "WidgetHandler", {
-      runtime: lambda.Runtime.NODEJS_10_X, // So we can use async in widget.js
-      code: lambda.Code.fromAsset('lambda'),
-      handler: 'plantinformation.handler',
-    });
-  }
+        // This should be provided during manual deployment or through CI/CID
+        const plantNameParameter = new CfnParameter(this, "plantName", {
+            type: "String",
+            allowedValues: ['neckarsulm', 'wolfsburg', 'osnabrueck'],
+            description: "The name of the plant to be deployed."
+        });
+
+
+        const appConfigApplication = new CfnApplication(this, 'PlantAppConfigApplication', {
+            name: 'PlantAppConfigApplication',
+            description: 'AppConfig Application for plants Demo'
+        });
+
+        const plantInformationLambda = new lambda.Function(this, "plantInformation", {
+            runtime: lambda.Runtime.NODEJS_14_X, // So we can use async in widget.js
+            code: lambda.Code.fromAsset('lambda'),
+            handler: 'plantinformation.handler',
+            environment: {
+                AWS_APPCONFIG_EXTENSION_HTTP_PORT: '2772',
+                AWS_APPCONFIG_EXTENSION_POLL_INTERVAL_SECONDS: '45',
+                AWS_APPCONFIG_EXTENSION_POLL_TIMEOUT_MILLIS: '3000',
+                AWS_APPCONFIG_REF: appConfigApplication.ref,
+                AWS_APPCONFIG_PLANT_NAME:plantNameParameter.value.toString(),
+            }
+        });
+
+        plantInformationLambda.addToRolePolicy(
+            new PolicyStatement({
+                resources: [
+                    `arn:aws:appconfig:${this.region}:${this.account}:*`
+                ],
+                actions: ['appconfig:*'],
+                effect: Effect.ALLOW
+            })
+        );
+
+    }
 }
