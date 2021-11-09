c585dcb86910ca0860af944aac8344ae5d8fc369
Kifah Abbad <kifah@web.de>
2021-11-09 16:23:35 +0100

    demo part for neackarsulm finished

--- lambda/plantinformation.ts
+++ lambda/plantinformation.ts
@@ -3,8 +3,8 @@ import * as AWS from 'aws-sdk';
 
 declare const process: {
     env: {
-        AWS_APPCONFIG_REF: string; // application id
         AWS_APPCONFIG_PLANT_NAME: string; // enviornment in AppConfig Application
+        AWS_APPCONFIG_APP_NAME: string; // name in AppConfig Application
     };
 };
 
@@ -13,16 +13,32 @@ const appConfig = new AWS.AppConfig({apiVersion: '2019-10-09'});
 export const handler = (
         event: any
     ): any => {
-        const params = {
-            MaxResults: 5,
+        const callParams = {
+            Application: process.env.AWS_APPCONFIG_APP_NAME,
+            Configuration: process.env.AWS_APPCONFIG_PLANT_NAME + 'DefaultConfig',
+            Environment: process.env.AWS_APPCONFIG_PLANT_NAME,
+            ClientId: createClientId()
         };
-        appConfig.listApplications(params, function (err, data) {
+
+        // debug call to list all applications
+        appConfig.getConfiguration(callParams, function (err, data) {
             console.log('application data...');
             if (err) console.log(err, err.stack);
-            else console.log(data);
+            else console.log(data.Content?.toString());
         });
+
         console.log('debugging plants configuration...');
-        console.log('application ref', process.env.AWS_APPCONFIG_REF);
+        console.log('application name', process.env.AWS_APPCONFIG_APP_NAME);
         console.log('application env', process.env.AWS_APPCONFIG_PLANT_NAME);
     }
 ;
+
+function createClientId() {
+    let dt = new Date().getTime();
+    const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
+}
--- lib/plants-stack.ts
+++ lib/plants-stack.ts
@@ -1,30 +1,22 @@
 import * as cdk from '@aws-cdk/core';
+import * as appconfig from '@aws-cdk/aws-appconfig';
 import * as lambda from '@aws-cdk/aws-lambda';
 import {CfnParameter} from "@aws-cdk/core";
 import {Effect, PolicyStatement} from '@aws-cdk/aws-iam';
 
-import {
-    CfnApplication,
-    CfnEnvironment,
-    CfnConfigurationProfile,
-    CfnHostedConfigurationVersion
-} from "@aws-cdk/aws-appconfig";
-
 export class PlantsStack extends cdk.Stack {
+
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
+
+        // the AppConfig Application has been created manually, and could be accessed by all stacks
+        const appConfigApplicationName='PlantAppConfigApplication';
 
         // This should be provided during manual deployment or through CI/CID
         const plantNameParameter = new CfnParameter(this, "plantName", {
             type: "String",
             allowedValues: ['neckarsulm', 'wolfsburg', 'osnabrueck'],
             description: "The name of the plant to be deployed."
-        });
-
-
-        const appConfigApplication = new CfnApplication(this, 'PlantAppConfigApplication', {
-            name: 'PlantAppConfigApplication',
-            description: 'AppConfig Application for plants Demo'
         });
 
         const plantInformationLambda = new lambda.Function(this, "plantInformation", {
@@ -35,11 +27,12 @@ export class PlantsStack extends cdk.Stack {
                 AWS_APPCONFIG_EXTENSION_HTTP_PORT: '2772',
                 AWS_APPCONFIG_EXTENSION_POLL_INTERVAL_SECONDS: '45',
                 AWS_APPCONFIG_EXTENSION_POLL_TIMEOUT_MILLIS: '3000',
-                AWS_APPCONFIG_REF: appConfigApplication.ref,
-                AWS_APPCONFIG_PLANT_NAME:plantNameParameter.value.toString(),
+                AWS_APPCONFIG_APP_NAME: appConfigApplicationName,
+                AWS_APPCONFIG_PLANT_NAME: plantNameParameter.value.toString(),
             }
         });
 
+        // allow access to Appconfig confugrations (these rights should be refined)
         plantInformationLambda.addToRolePolicy(
             new PolicyStatement({
                 resources: [
